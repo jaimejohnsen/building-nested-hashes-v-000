eb5bf5b3f78c0b1402c036cf6898b1a4f77c5f4c
Jaime Johnsen <drivetraindiva@gmail.com>
2018-07-25 01:55:25 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,11 +3,7 @@ def third_challenge
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo",
-        :age => "15",
-        :status => "alive"
-        #your key/value pairs here
-        },
+      :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => []
    },
    :capulet => {
